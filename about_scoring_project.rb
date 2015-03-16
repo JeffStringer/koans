a7cd7aedcc25ab1b5a62f337bf0e1af249582fd2
jeffstringer <jeff.j.stringer@gmail.com>
2015-03-15 20:57:23 -0700

    add about_scoring_project.rb

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -31,6 +31,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 def score(dice)
   # You need to write this method
+  score = 0
+  dice_count = dice.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+  lowers_present = dice.any? { |die| [2,3,4,6].include?(die) }
+  [1,5].each do |n|
+    if dice_count[n] >= 3
+      n == 5 ? score += 500 : score += 1000
+      dice_count[n] = dice_count[n] - 3
+    end
+    n == 5 ? score += ( dice_count[5] * 50 ) : score += ( dice_count[1] * 100 )
+  end
+  [2,3,4,6].each { |lower| score += lower * 100 if dice_count[lower] >= 3 } if lowers_present
+  score
 end
 
 class AboutScoringProject < Neo::Koan
@@ -46,24 +58,24 @@ class AboutScoringProject < Neo::Koan
     assert_equal 100, score([1])
   end
 
-  def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-    assert_equal 300, score([1,5,5,1])
-  end
-
-  def test_score_of_single_2s_3s_4s_and_6s_are_zero
-    assert_equal 0, score([2,3,4,6])
-  end
-
-  def test_score_of_a_triple_1_is_1000
-    assert_equal 1000, score([1,1,1])
-  end
-
   def test_score_of_other_triples_is_100x
     assert_equal 200, score([2,2,2])
     assert_equal 300, score([3,3,3])
     assert_equal 400, score([4,4,4])
     assert_equal 500, score([5,5,5])
     assert_equal 600, score([6,6,6])
+  end
+
+  def test_score_of_a_triple_1_is_1000
+    assert_equal 1000, score([1,1,1])
+  end
+
+  def test_score_of_single_2s_3s_4s_and_6s_are_zero
+    assert_equal 0, score([2,3,4,6])
+  end
+
+  def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
+    assert_equal 300, score([1,5,5,1])
   end
 
   def test_score_of_mixed_is_sum
